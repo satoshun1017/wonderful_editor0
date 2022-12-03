2cb138087834700747a4f30cf3f018133c298863
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2022-12-03 11:09:43 +0900

    Task.EX終了

--- config/application.rb
+++ config/application.rb
@@ -28,23 +28,21 @@ module WonderfulEditor0
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.api_only = true
   end
-
-  config.generators do |g|
-    g.template_engine false
-    g.javascripts false
-    g.stylesheets false
-    g.helper false
-    g.test_framework :rspec,
-                     view_specs: false,
-                     routing_specs: false,
-                     helper_specs: false,
-                     controller_specs: false,
-                     request_specs: true
-  end
-
-  config.api_only = true
 end
