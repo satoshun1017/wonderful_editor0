18a82c9c37e08d6a9c542843cf819c4df429ec5a
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2022-12-03 10:13:23 +0900

    Task5　終了

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -32,4 +32,19 @@ module WonderfulEditor0
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
+
+  config.generators do |g|
+    g.template_engine false
+    g.javascripts false
+    g.stylesheets false
+    g.helper false
+    g.test_framework :rspec,
+                     view_specs: false,
+                     routing_specs: false,
+                     helper_specs: false,
+                     controller_specs: false,
+                     request_specs: true
+  end
+
+  config.api_only = true
 end
