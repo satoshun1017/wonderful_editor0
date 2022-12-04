c9d6ad6276404b2476e5b56ddd5f09c6ad3ab3c2
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2022-12-04 10:16:35 +0900

    テストを実装する前にコメントアウト

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -16,9 +16,9 @@
 # Foreign Keys
 #
 #  fk_rails_...  (user_id => users.id)
-#
-require "rails_helper"
+# #
+# require "rails_helper"
 
-RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Article, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
